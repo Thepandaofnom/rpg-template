a1b6977226e8c4c8c6272835f575f0020ace5a8c
Thepandaofnom <andrewjzambik@yahoo.com>
2026-08-17 07:32:37 -0400

    Creating new branch for Sami to create pixle art in

---
+++ plugins/SubmappingExPlugin/events/eventGetBackgroundTileAttribute.js
@@ -0,0 +1,64 @@
+const l10n = require("../helpers/l10n").default;
+
+export const id = "EVENT_GET_BACKGROUND_TILE_ATTR";
+export const name = "Get background tile attribute";
+export const groups = ["EVENT_GROUP_SCREEN"];
+
+export const autoLabel = (fetchArg) => {
+  return `Get background tile attribute`;
+};
+
+export const fields = [
+  {
+    key: `x`,
+    label: "X",
+    type: "value",
+    width: "50%",
+    defaultValue: {
+      type: "number",
+      value: 0,
+    },
+  },
+  {
+    key: `y`,
+    label: "Y",
+    type: "value",
+    width: "50%",
+    defaultValue: {
+      type: "number",
+      value: 0,
+    },
+  },
+  {
+    key: "variable",
+    label: l10n("FIELD_VARIABLE"),
+    description: l10n("FIELD_VARIABLE_DESC"),
+    type: "variable",
+    defaultValue: "LAST_VARIABLE",
+  }
+];
+
+export const compile = (input, helpers) => {
+  const { _callNative, _stackPop, _addComment, _declareLocal, getVariableAlias, _stackPushConst, _isIndirectVariable, _setInd, _stackPushScriptValue } = helpers;
+
+  const variableAlias = getVariableAlias(input.variable);
+  let dest = variableAlias;
+  if (_isIndirectVariable(input.variable)) {
+    const index_result = _declareLocal("index_result", 1, true);
+    dest = index_result;
+  }
+  
+  _addComment("Get background tile attribute");
+
+  _stackPushConst(dest);
+  _stackPushScriptValue(input.y);
+  _stackPushScriptValue(input.x);
+
+  _callNative("vm_get_background_attribute_tile");
+  _stackPop(3);
+  
+  if (_isIndirectVariable(input.variable)) {
+    _setInd(variableAlias, dest);
+  }
+
+};
